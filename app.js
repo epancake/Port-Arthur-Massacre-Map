68bdf49c82f6debd3fa20f4e7993c3528195a4c6
Emily Pancake <epancake24@gmail.com>
2018-03-05 22:12:52 -0700

    slider

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var map = L.map("map").setView([-43.041533, 147.651459], 10);
+var map = L.map("map").setView([-43.001533, 147.651459], 10);
 mapLink =
    '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 L.tileLayer(
@@ -7,6 +7,7 @@ L.tileLayer(
     maxZoom: 18,
   }).addTo(map);
 let currentCircleId
+let circlePos = 0
 
 /* Initialize the SVG layer */
 map._initPathRoot()
@@ -51,32 +52,9 @@ d3.json("./circles.json", function(collection) {
 
 
   function handleMouseOver(d) {
-    console.log("did", d.circle.id)
-    currentCircleId = d.circle.id
     d3.select(this).attr({
       r: 30,
-      id: d.circle.id,
     })
-    map.setView(d.circle.coordinates, d.circle.zoom)
-    document.getElementById("storyTitle").innerHTML = d.circle.title
-    document.getElementById("storyDescription").innerHTML = d.circle.description
-    document.getElementById("storyImage1").src = d.circle.image1
-    document.getElementById("storyImage2").src = d.circle.image2
-    addBtn()
-    addResetBtn()
-
-  }
-
-  function addBtn () {
-    $aside = document.getElementById("storyCard")
-    if (!document.getElementById("nextBtn")) {
-      var btn = document.createElement("button")
-      var t = document.createTextNode("Next")
-      btn.setAttribute("id", "nextBtn")
-      btn.appendChild(t);
-      btn.addEventListener("click", moveToNext);
-      document.getElementById("storyCard").appendChild(btn)
-    }
   }
 
   function addResetBtn () {
@@ -95,30 +73,144 @@ d3.json("./circles.json", function(collection) {
     map.setView([-43.141533, 147.651459], 10)
   }
 
-  function moveToNext () {
-    console.log("currentCircleId", currentCircleId)
-    d3.json("./circles.json", function(collection) {
-      collection.objects.forEach(function(d) {
-        if (d.circle.id === currentCircleId + 1) {
-          map.setView(d.circle.coordinates, d.circle.zoom)
-          document.getElementById("storyTitle").innerHTML = d.circle.title
-          document.getElementById("storyDescription").innerHTML = d.circle.description
-          document.getElementById("storyImage1").src = d.circle.image1
-          document.getElementById("storyImage2").src = d.circle.image2
-
-          addBtn()
-        }
-      })
-      currentCircleId++
-    })
-    // map.setView(d.circle.coordinates, d.circle.zoom)
-
-  }
-
-
   function handleMouseOut(d, i) {
     d3.select(this).attr({
       r: 20
     });
   }
 })
+
+//////////
+
+var margin = {top: 50, right: 50, bottom: 50, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 100 - margin.bottom - margin.top;
+
+var svg1 = d3.select("#slider3").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var x = d3.scale.linear()
+    .domain([0, 11])
+    .range([0, width])
+    .clamp(true);
+
+var brush = d3.svg.brush()
+    .x(x)
+    .extent([0, 0])
+    .on("brush", brushed)
+    .on("brushend", brushend);
+
+svg1.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height / 2 + ")")
+    .call(d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
+      .tickFormat(function(d) { return d})
+      .tickSize(1)
+      .tickPadding(10))
+  .select(".domain")
+  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "halo");
+
+var slider = svg1.append("g")
+    .attr("class", "slider")
+    .call(brush);
+
+slider.selectAll(".extent,.resize")
+    .remove();
+
+slider.select(".background")
+    .attr("height", height);
+
+var handle = slider.append("circle")
+    .attr("class", "handle")
+    .attr("transform", "translate(0," + height / 2 + ")")
+    .attr("r", 9);
+
+slider
+    .call(brush.event)
+  .transition() // gratuitous intro!
+    .duration(750)
+    // .call(brush.extent([70, 70]))
+    .call(brush.event);
+
+function brushed() {
+  var value = brush.extent()[0];
+
+  if (d3.event.sourceEvent) { // not a programmatic event
+    value = x.invert(d3.mouse(this)[0]);
+    brush.extent([value, value]);
+  }
+
+  handle.attr("cx", x(value));
+  // d3.select("body").style("background-color", d3.hsl(value, .8, .8));
+}
+
+function brushend() {
+   if (!d3.event.sourceEvent) {
+     return; // only transition after input
+   }
+
+   var value = brush.extent()[0];
+   brush.extent([value, value]);
+
+   d3.select(this)
+     .transition()
+     .duration(0)
+     .call(brush.event);
+
+   d3.select(this)
+     .transition()
+     .call(brush.extent(brush.extent().map(function(d) { circlePos = d3.round(d); console.log('circlePos', circlePos); handleBrushing(circlePos); return d3.round(d); })))
+     .call(brush.event);
+ }
+
+function handleBrushing(sliderPosition) {
+  d3.json("./circles.json", function(collection) {
+    collection.objects.forEach(function(d) {
+      if (d.circle.id === sliderPosition) {
+        document.getElementById("storyTitle").innerHTML = d.circle.title
+        document.getElementById("storyDescription").innerHTML = d.circle.description
+        document.getElementById("storyImage1").src = d.circle.image1
+        document.getElementById("storyImage2").src = d.circle.image2
+        document.getElementsByTagName("circle")[0].style.fill = d.circle.color
+        map.setView(d.circle.coordinates, d.circle.zoom)
+      }
+    })
+    currentCircleId = sliderPosition
+  })
+}
+
+function addBtn () {
+  $aside = document.getElementById("storyCard")
+  if (!document.getElementById("nextBtn")) {
+    var btn = document.createElement("button")
+    var t = document.createTextNode("Next")
+    btn.setAttribute("id", "nextBtn")
+    btn.appendChild(t);
+    btn.addEventListener("click", moveToNext);
+    document.getElementById("storyCard").appendChild(btn)
+  }
+}
+
+function moveToNext () {
+  console.log("currentCircleId", currentCircleId)
+  d3.json("./circles.json", function(collection) {
+    collection.objects.forEach(function(d) {
+      if (d.circle.id === currentCircleId + 1) {
+        map.setView(d.circle.coordinates, d.circle.zoom)
+        document.getElementById("storyTitle").innerHTML = d.circle.title
+        document.getElementById("storyDescription").innerHTML = d.circle.description
+        document.getElementById("storyImage1").src = d.circle.image1
+        document.getElementById("storyImage2").src = d.circle.image2
+      }
+    })
+    currentCircleId++
+  })
+  // map.setView(d.circle.coordinates, d.circle.zoom)
+
+}
